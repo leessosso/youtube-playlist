747bc9e66e6739258469fdd6388f0f50a748914d
leessosso <ssossoking@naver.com>
2025-03-26 22:50:01 +0900

    fix: YouTubeCallback 경로를 '/youtube-playlist/youtube-callback'으로 수정하여 라우팅 개선

--- src/App.tsx
+++ src/App.tsx
@@ -12,7 +12,7 @@ function App() {
       <BrowserRouter basename={import.meta.env.BASE_URL}>
         <Routes>
           <Route path="/" element={<YouTubeExtractor />} />
-          <Route path="youtube-callback" element={<YouTubeCallback />} />
+          <Route path="youtube-playlist/youtube-callback" element={<YouTubeCallback />} />
         </Routes>
       </BrowserRouter>
     </ChakraProvider>
