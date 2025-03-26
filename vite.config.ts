4194580a329da7c68dbfa00eb509b175f437b293
leessosso <ssossoking@naver.com>
2025-03-26 22:56:30 +0900

    feat: 404 페이지 추가 및 Vite 설정에서 리다이렉트 경로 구성

--- vite.config.ts
+++ vite.config.ts
@@ -5,4 +5,12 @@ import react from '@vitejs/plugin-react'
 export default defineConfig({
   plugins: [react()],
   base: process.env.NODE_ENV === 'production' ? '/youtube-playlist/' : '/',
+  build: {
+    rollupOptions: {
+      input: {
+        main: 'index.html',
+        notFound: '404.html'
+      }
+    }
+  }
 })
