b53630e83bfa6d76c7a151b9a43c5dd20b391de1
leessosso <ssossoking@naver.com>
2025-03-26 23:56:14 +0900

    fix: 404 페이지의 리다이렉션 로직을 개선하여 홈으로 리다이렉트하면서 쿼리 파라미터를 유지하도록 수정함

--- src/App.tsx
+++ src/App.tsx
@@ -1,20 +1,36 @@
-import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import { BrowserRouter as Router, Routes, Route, Navigate } from 'react-router-dom';
 import { ChakraProvider, createStandaloneToast } from '@chakra-ui/react';
 import YouTubeExtractor from './pages/YouTubeExtractor';
-import YouTubeCallback from './pages/YouTubeCallback';
+import { useEffect } from 'react';
+import { handleAuthCallback } from './utils/youtubeAuth';
 
 const { ToastContainer } = createStandaloneToast();
 
 function App() {
+  useEffect(() => {
+    // URL에 code 파라미터가 있으면 인증 처리
+    const urlParams = new URLSearchParams(window.location.search);
+    const code = urlParams.get('code');
+    if (code) {
+      handleAuthCallback(code).then(success => {
+        if (success) {
+          // 인증 처리 후 URL에서 code 파라미터 제거
+          const newUrl = window.location.pathname;
+          window.history.replaceState({}, '', newUrl);
+        }
+      });
+    }
+  }, []);
+
   return (
     <ChakraProvider>
       <ToastContainer />
-      <BrowserRouter basename={import.meta.env.BASE_URL}>
+      <Router basename="/youtube-playlist">
         <Routes>
           <Route path="/" element={<YouTubeExtractor />} />
-          <Route path="youtube-callback" element={<YouTubeCallback />} />
+          <Route path="*" element={<Navigate to="/" replace />} />
         </Routes>
-      </BrowserRouter>
+      </Router>
     </ChakraProvider>
   );
 }
