c080a0a9ca664bbb4e1ae25e0f7ec1f9d7d39aaa
leessosso <ssossoking@naver.com>
2025-03-26 20:26:20 +0900

    개발 환경과 프로덕션 환경의 base URL 설정 분리

--- vite.config.ts
+++ vite.config.ts
@@ -4,5 +4,5 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/youtube-playlist/', // GitHub Pages 배포를 위한 base 경로 설정
+  base: process.env.NODE_ENV === 'production' ? '/youtube-playlist/' : '/',
 })
